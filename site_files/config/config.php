0675e792e1cc63f86863bd642e07fc98d3a77f8e
Taylor Jones <taylor@whiteboard.is>
2012-12-29 10:32:40 -0500

    Removed URL from config.php

--- site_files/config/config.php
+++ site_files/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://wb-passion-test-env.elasticbeanstalk.com';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
