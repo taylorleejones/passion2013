0cc716b7a9acbff58357e37c7baae02870c489ec
Taylor Jones <taylor@whiteboard.is>
2012-12-28 22:07:46 -0500

    Removed old files & setup blank codeigniter

--- site_files/views/testing/hello_world.php
+++
@@ -1,6 +0,0 @@
-<?php echo date("U"); ?>
-<br /><br />
-Time: <?php echo $this->benchmark->elapsed_time(); ?>
-<br />Mem: <?php echo $this->benchmark->memory_usage(); ?>
-<br /><br />
-<div class="json-output"></div>
