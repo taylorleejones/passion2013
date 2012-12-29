48aeadf3d21a05e56b3f3f5c4a2a915e46244327
Taylor Jones <taylor@whiteboard.is>
2012-12-28 21:20:51 -0500

    Edited config file URL

--- system_files/core/Config.php
+++ system_files/core/Config.php
@@ -75,7 +75,7 @@ class CI_Config {
 
 			else
 			{
-				$base_url = 'http://localhost/';
+				$base_url = 'http://passionlive.whiteboardnetwork.com';
 			}
 
 			$this->set_item('base_url', $base_url);
