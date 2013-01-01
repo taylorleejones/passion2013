56d00797778a96cbde5250f4b53a6baf8f270501
Taylor Jones <taylor@whiteboard.is>
2013-01-01 16:38:00 -0500

    Added Google Analytics for two accounts

--- site_files/views/footer.php
+++ site_files/views/footer.php
@@ -18,23 +18,27 @@
 
 
 
-        <script type="text/javascript">
+<script type="text/javascript">
 
-          var _gaq = _gaq || [];
-          _gaq.push(['_setAccount', 'UA-37348657-1']);
-          _gaq.push(['_trackPageview']);
+  var _gaq = _gaq || [];
 
-          _gaq.push(['_setAccount', 'UA-37198744-4']);
-          _gaq.push(['_trackPageview']);
+// First UA code
+  _gaq.push(['_setAccount', 'UA-37198744-4']);
+  _gaq.push(['_trackPageview']);
+  _gaq.push(['_trackPageLoadTime']);
 
+// Second UA code
+  _gaq.push(['b._setAccount', 'UA-37348657-1']);
+  _gaq.push(['b._trackPageview']);
+  _gaq.push(['b._trackPageLoadTime']);
 
-          (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-          })();
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 
-        </script>
+</script>
         
     </body>
 </html>
